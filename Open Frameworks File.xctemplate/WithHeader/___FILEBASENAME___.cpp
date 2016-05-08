e22384596c03bbebc3a7133342b899bf920b5227
貴田 達也 <tatsuya.kida@i-studio.co.jp>
2016-05-09 04:07:36 +0900

    initial commit

---
+++ Open Frameworks File.xctemplate/WithHeader/___FILEBASENAME___.cpp
@@ -0,0 +1,31 @@
+//  who  : ___FULLUSERNAME___
+
+#pragma once
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+private: 
+
+
+public:
+	___FILEBASENAMEASIDENTIFIER___()
+	{
+
+	}
+
+	~___FILEBASENAMEASIDENTIFIER___()
+	{
+
+
+	}
+
+	setup()
+	{
+
+	}
+
+	update()
+	{
+		
+	}
+
+};
---
+++ Open Frameworks File.xctemplate/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,15 @@
+//  what : <#purpose#>
+//  who  : ___FULLUSERNAME___
+
+#pragma once
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+private: 
+
+
+public:
+	___FILEBASENAMEASIDENTIFIER___();
+	~___FILEBASENAMEASIDENTIFIER___();
+	setup();
+	update();
+};
